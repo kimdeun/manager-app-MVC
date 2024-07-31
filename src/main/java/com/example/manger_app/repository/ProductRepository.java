f81452764b3620cbaec5e20588e8dd87d0027a71
De Un Kim <deunkim@MacbookM1.local>
2024-07-31 08:03:29 +0300

    Spring Boot, Spring MVC, lombok, thymeleaf, validation

---
+++ src/main/java/com/example/manger_app/repository/ProductRepository.java
@@ -0,0 +1,16 @@
+package com.example.manger_app.repository;
+
+import com.example.manger_app.entity.Product;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface ProductRepository {
+    List<Product> findAll();
+
+    Product save(Product product);
+
+    Optional<Product> findById(Integer productId);
+
+    void deleteById(Integer id);
+}
---
+++ src/main/java/com/example/manger_app/service/ProductService.java
@@ -0,0 +1,18 @@
+package com.example.manger_app.service;
+
+import com.example.manger_app.entity.Product;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface ProductService {
+    List<Product> findAllProducts();
+
+    Product createProduct(String title, String details);
+
+    Optional<Product> findProduct(int productId);
+
+    void updateProduct(Integer id, String title, String details);
+
+    void deleteProduct(Integer id);
+}
